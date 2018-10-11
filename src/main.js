57f8dc211cd94704c03dc00e6e15e6351266dda6
Chan Lee <chanethanlee@gmail.com>
2018-10-10 22:09:01 -0700

    edit weekday calculator algo and modify front-end accordingly.

--- src/main.js
+++ src/main.js
@@ -2,19 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import { Weekday} from './weekday.js';
+import { Weekday} from './weekday.js';
 
 $(document).ready(function() {
   $("#form").submit(function(event){
     event.preventDefault();
     let date = $("#year").val();
-    let weekdayInstance = new Date(date);
-    let weekdayInt = weekdayInstance.getDay();
-    // let weekdayString = Weekday.prototype.weekdayTranslator(weekdayInt);
-    function dayOfWeekAsString(dayIndex) {
-      return ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"][dayIndex];
-    }
-    let result = dayOfWeekAsString(weekdayInt);
-    $("#result").text(`${result}`);
+    let dateArray = date.split("-");
+    console.log(typeof dateArray[0]);
+    let weekdayInstance = new Weekday(dateArray[0],dateArray[1],dateArray[2]);
+    let weekdayInt = weekdayInstance.calculateWeekday();
+    let weekdayString = Weekday.prototype.weekdayTranslator(weekdayInt);
+    $("#result").text(`${weekdayString}`);
   });
 });
--- src/weekday.js
+++ src/weekday.js
@@ -1,55 +1,68 @@
-// export class Weekday {
-//   constructor(day, month, year) {
-//     this.day = day;
-//     this.month = month;
-//     this.year = year;
-//   }
-//
-//   calculateWeekday(){
-//     let m = this.month;
-//     let d = this.day;
-//     let y = this.year;
-//
-//     let monthTable = [1, 4, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5];
-//     m = monthTable[m - 1];
-//
-//     let centuryTable = [0, 5, 3, 1];
-//     let c = centuryTable[Math.floor(y / 100) % 4];
-//
-//     if (m > 3) {
-//         y -= 1;
-//     }
-//
-//     console.log(m);
-//     y = y % 100;
-//     y = (Math.floor((y / 4)) + y) % 7;
-//     let weekdayInt = (d + m + c + y) % 7;
-//     return weekdayInt;
-//   }
-//
-//   weekdayTranslator(weekdayInt){
-//     let weekday;
-//     if(weekdayInt == 0) {
-//       weekday = "Saturday";
-//     }
-//     else if(weekdayInt == 1) {
-//       weekday = "Sunday";
-//     }
-//     else if(weekdayInt ==2) {
-//       weekday = "Monday";
-//     }
-//     else if(weekdayInt == 3) {
-//       weekday = "Tuesday";
-//     }
-//     else if(weekdayInt == 4) {
-//       weekday = "Wednesday";
-//     }
-//     else if(weekdayInt == 5) {
-//       weekday = "Thursday";
-//     }
-//     else  {
-//       weekday = "Friday";
-//     }
-//     return weekday;
-//   }
-// }
+export class Weekday {
+  constructor(year, month, day) {
+    this.year = parseInt(year);
+    this.month = parseInt(month);
+    this.day = parseInt(day);
+  }
+
+  calculateWeekday(){
+    let m = this.month;
+    let d = this.day;
+    let y = this.year;
+    let c = Math.floor(y / 100);
+    let w = 0;
+
+    console.log(m);
+    console.log(d);
+    console.log(y);
+    console.log(c);
+
+    // Determine Y, which is the year minus 1 for  January or February, and the year for  the rest of the year
+    if (m < 3) {
+      y--;
+    }
+
+    // Determine y, which is the last 2 digits of Y
+    y = y % 100;
+
+    // Determine m, which is the month shifted left by 2 (March = 1,... February = 12)
+    if (m-2<1) {
+      m += 10;
+    } else {
+      m -=2;
+    }
+
+    let temp = d + Math.floor((2.6*m) - 0.2) + y + Math.floor(y / 4) + Math.floor(c / 4) - (2*c);
+    console.log(temp);
+    w =  temp % 7;
+
+    console.log(w);
+    return w;
+  }
+
+  weekdayTranslator(weekdayInt){
+    let weekday;
+    if(weekdayInt == 0) {
+      weekday = "Sunday";
+    }
+    else if(weekdayInt == 1) {
+      weekday = "Monday";
+    }
+    else if(weekdayInt ==2) {
+      weekday = "Tuesday";
+    }
+    else if(weekdayInt == 3) {
+      weekday = "Wednesday";
+    }
+    else if(weekdayInt == 4) {
+      weekday = "Thursday";
+    }
+    else if(weekdayInt == 5) {
+      weekday = "Friday";
+    }
+    else  {
+      weekday = "Saturday";
+    }
+    return weekday;
+  }
+}
